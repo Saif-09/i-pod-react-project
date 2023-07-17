d2b3303b6af14352c1451028add49f634aedbe7b
Oreo <oreo@Adibas-MacBook-Air.local>
2023-07-17 17:48:27 +0530

    initial

---
+++ src/components/stateful/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Ipod from "../stateless/Ipod";
+import ZingTouch from "zingtouch";
+import Helmet from "react-helmet";
+
+//importing images from assets
+import images from "../../assets/images/images";
+// importing songs and thumbnails
+import songs from "../../assets/songs/songs";
